cfa4d89dc15bc815cb98cc14f6cd868766542c42
phhhh0 <148896226+phhhh0@users.noreply.github.com>
2025-04-23 08:51:45 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
-using CarCRUD.Data; // Corrigido para CarCRUD.Data
-using CarCRUD.Routes; // Corrigido para CarCRUD.Routes
+using CarCRUD.Data; 
+using CarCRUD.Routes; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Adicionar serviços ao contêiner.
+
 builder.Services.AddOpenApi();
-builder.Services.AddScoped<ContextoCarro>(); // Corrigido para ContextoCarro
+builder.Services.AddScoped<ContextoCarro>(); 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configurar o pipeline de requisições HTTP.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -20,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.RotaCarro(); // Corrigido para RotaCarro
+app.RotaCarro(); 
 
 app.UseHttpsRedirection();
 app.Run();

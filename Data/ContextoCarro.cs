1a81ccd34dcf1be63376b369111f7a4e036c144a
phhhh0 <148896226+phhhh0@users.noreply.github.com>
2025-04-23 08:46:31 -0300

    Update ContextoCarro.cs

--- Data/ContextoCarro.cs
+++ Data/ContextoCarro.cs
@@ -1,18 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using CarCRUD.Models; // Alterado para CarCRUD.Models, pois você está trabalhando com carros
+using CarCRUD.Models;
 
-namespace CarCRUD.Data // Corrigido para CarCRUD.Data
+namespace CarCRUD.Data 
 {
-    // Contexto de banco de dados para Carro
+    
     public class ContextoCarro : DbContext
     {
-        // Tabela de carros
+       
         public DbSet<CarroModelo> Carros { get; set; }
 
-        // Configuração da conexão com o banco de dados
+       
         protected override void OnConfiguring(DbContextOptionsBuilder construtorOpcoes)
         {
-            construtorOpcoes.UseSqlite("Data Source=Carro.sqlite"); // Usando o SQLite como banco de dados
+            construtorOpcoes.UseSqlite("Data Source=Carro.sqlite"); 
             base.OnConfiguring(construtorOpcoes);
         }
     }
